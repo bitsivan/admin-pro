b88424c5492178e4a2e727dbde34fd14dbf90ab9
ivan montejo <sergio.montejo@improving.com>
2021-12-16 20:37:51 -0600

    added routes page components children

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BreadcrumbComponent } from './share/breadcrumb/breadcrumb.component';
 import { HeaderComponent } from './share/header/header.component';
 import { SidebarComponent } from './share/sidebar/sidebar.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +23,15 @@ import { SidebarComponent } from './share/sidebar/sidebar.component';
     DashboardComponent,
     BreadcrumbComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
